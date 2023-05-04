9bc1f8513f1dec0f9273ba96596c505f3b10adbb
amigin <amigin@gmail.com>
2023-05-04 22:38:27 +0300

    Debugging

--- src/grpc_client/generate.rs
+++ src/grpc_client/generate.rs
@@ -52,12 +52,12 @@ pub fn generate(
       #ast
 
       impl #struct_name{
-        pub fn new(get_grpc_address: Arc<dyn GrpcClientSettings + Send + Sync + 'static>) -> Self {
+        pub async fn new(get_grpc_address: Arc<dyn GrpcClientSettings + Send + Sync + 'static>,) -> Self {
             Self {
                 grpc_channel: GrpcChannel::new(
                     get_grpc_address,
-                    GRPC_SERVICE_NAME,
-                    Duration::from_secs(#timeout_sec),
+                    Arc::new(MyGrpcServiceFactory),
+                    Duration::from_secs(10),
                 ),
             }
         }
